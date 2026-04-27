25071813160cfe5690177d088b7cb5c22e54a3f8
JasonEvan <7ason3van@gmail.com>
2026-04-27 21:17:34 +0700

    feat: make the logout button works

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,17 +1,38 @@
+"use client";
+
 import { LogOut } from "lucide-react";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
 import { SidebarTrigger } from "../ui/sidebar";
 import { ThemeToggle } from "./ThemeToggle";
+import api from "@/lib/axios";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Topbar() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await api.post("/auth/logout");
+      toast.success("Berhasil logout");
+      router.push("/login");
+    } catch (error) {
+      toast.error("Gagal logout");
+    }
+  };
+
   return (
     <header className="flex h-14 items-center gap-2 border-b border-border bg-background px-4">
       <SidebarTrigger />
       <Separator orientation="vertical" className="h-6" />
       <div className="ml-auto flex items-center gap-2">
         <ThemeToggle />
-        <Button className="text-destructive" variant="ghost">
+        <Button
+          className="text-destructive cursor-pointer"
+          variant="ghost"
+          onClick={handleLogout}
+        >
           <LogOut className="mr-2 h-4 w-4" />
           Logout
         </Button>
