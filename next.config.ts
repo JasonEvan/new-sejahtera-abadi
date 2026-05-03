4e2b2189fe24caaf7702e41709742a9a75ebb74b
JasonEvan <7ason3van@gmail.com>
2026-05-03 12:15:31 +0700

    build: ignore eslint when build process

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  // @ts-expect-error - This is a valid property in Next.js, but TypeScript doesn't recognize it yet.
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
